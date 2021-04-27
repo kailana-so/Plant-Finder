1e69de6683eef680da38b6e22c08d0f6b45b3106
kailana-so <kailana.work@gmail.com>
2021-04-27 15:24:16 +1000

    added pg as the client

--- libs/db.js
+++ libs/db.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV) {
   var knex = require('knex')({
-    connectionString: process.env.DATABASE_URL,
+        client: 'pg',
+        connectionString: process.env.DATABASE_URL,
     })
 } else {
     var knex = require('knex')({
